6a6ccbb40e8846c94b412cad00e932f1cfce644e
Shyam Kumar <kumar.sh@husky.neu.edu>
2019-09-19 17:36:08 +0000

    Modifying service file to run automatically

--- start.sh
+++ start.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-if [[ "x$PROD" == "x" ]]; then 
-	echo "This script is for starting in production."
-	echo "Use"
-	echo "   mix phx.server"
-	exit
-fi
-
-# TODO: Enable this script by removing the above.
-
 export MIX_ENV=prod
 export PORT=4790
 
@@ -18,8 +9,5 @@ _build/prod/rel/practice/bin/practice stop || true
 
 echo "Starting app..."
 
-_build/prod/rel/practice/bin/practice foreground
-
-# TODO: Add a systemd service file
-#       to start your app on system boot.
+_build/prod/rel/practice/bin/practice start
 
